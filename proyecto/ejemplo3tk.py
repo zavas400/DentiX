d4df0c49f6c101c7501be2051464f59a571a2f1c
zavas400 <luisantiagozamora@gmail.com>
2021-05-16 17:22:52 -0500

    tkinter ejemplos

---
+++ proyecto/ejemplo3tk.py
@@ -0,0 +1,70 @@
+from tkinter import *
+
+# creamos la ventana
+ventana = .Tk()
+ventana.title("DentiX")
+ventana.geometry("500x700")
+
+
+boton1 = .Button(text="Ingresar")
+boton1.grid(column=0, row=0)
+
+boton2 = .Button(text="Ingresar")
+boton2.grid(column=1, row=0)
+
+boton3 = .Button(text="Ingresar")
+boton3.grid(column=2, row=0)
+
+boton4 = .Button(text="Ingresar")
+boton4.grid(column=3, row=0)
+
+boton5 = .Button(text="Ingresar")
+boton5.grid(column=4, row=0)
+
+boton6 = .Button(text="Ingresar")
+boton6.grid(column=0, row=1)
+
+boton7 = .Button(text="Ingresar")
+boton7.grid(column=1, row=1)
+
+boton8 = .Button(text="Ingresar")
+boton8.grid(column=2, row=1)
+
+boton9 = .Button(text="Ingresar")
+boton9.grid(column=3, row=1)
+
+boton10 = .Button(text="Ingresar")
+boton10.grid(column=4, row=1)
+
+boton11 = .Button(text="Ingresar")
+boton11.grid(column=0, row=2)
+
+boton12 = .Button(text="Ingresar")
+boton12.grid(column=1, row=2)
+
+boton13 = .Button(text="Ingresar")
+boton13.grid(column=2, row=2)
+
+boton14 = .Button(text="Ingresar")
+boton14.grid(column=3, row=2)
+
+boton15 = .Button(text="Ingresar")
+boton15.grid(column=4, row=2)
+
+boton16 = .Button(text="Ingresar")
+boton16.grid(column=0, row=3)
+
+boton17 = .Button(text="Ingresar")
+boton17.grid(column=1, row=3)
+
+boton18 = .Button(text="Ingresar")
+boton18.grid(column=2, row=3)
+
+boton19 = .Button(text="Ingresar")
+boton19.grid(column=3, row=3)
+
+boton20 = .Button(text="Ingresar")
+boton20.grid(column=4, row=3)
+
+
+ventana.mainloop()
--- proyecto/tkinter.py
+++ proyecto/tkinter.py
@@ -1,6 +1,17 @@
+from tkinter import *
 
-# aaaaaaaaaaaaaaa
+# creamos la ventana
+ventana = .Tk()
+ventana.title("DentiX")
+ventana.geometry("500x700")
 
+
+
+
+
+
+
+ventana.mainloop()
 
 
 
