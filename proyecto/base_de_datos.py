ed0e9f7466c0c82d336144c645fdb9bf2c7ec14e
zavas400 <luisantiagozamora@gmail.com>
2021-05-16 20:15:53 -0500

    bla

--- proyecto/base_de_datos.py
+++ proyecto/base_de_datos.py
@@ -12,3 +12,25 @@ def datos(salida, seleccion):
         for fila in lector:
             print(fila[0], fila[seleccion])
     archivo.close()
+
+def datoss(salida, seleccion):
+    archivo = open(direct, "r")
+    lector = csv.reader(archivo)
+    if salida == 1:
+        contador = 0
+        for fila in lector:
+            if contador == 0:
+                Nombre = fila[seleccion]
+            elif contador == 1:
+                RFC = fila[seleccion]
+            elif contador == 2:
+                Cedula_federal = fila[seleccion]
+            elif contador == 3:
+                Telefono_personal = fila[seleccion]
+            elif contador == 4:
+                Telefono_consultorio = fila[seleccion]
+                break
+
+        contador = contador +1
+    archivo.close()
+    return Nombre, RFC, Cedula_federal
--- proyecto/tkinter1.py
+++ proyecto/tkinter1.py
@@ -1,9 +1,32 @@
 from tkinter import *
 
+
+def openNewWindow():
+    # Toplevel object which will
+    # be treated as a new window
+    newWindow = Toplevel(ventana)
+
+    # sets the title of the
+    # Toplevel widget
+    newWindow.title("New Window")
+
+    # sets the geometry of toplevel
+    newWindow.geometry("200x200")
+
+    # A Label widget to show in toplevel
+    Label(newWindow,text="This is a new window").pack()
+
+
+label = Label(ventana,text="This is the main window")
+
 # creamos la ventana
 ventana = Tk()
 ventana.title("DentiX")
 ventana.geometry("500x700")
+
+btn = Button(ventana,text="Click to open a new window",command=openNewWindow)
+
+
 
 
 
